b47c796c98cd6cf1afa78dd5dc8e49aeff59f225
DeeJayTC <DeeJay@tcdev.de>
2020-02-18 00:04:57 +0100

    CORS fix, some more stuff

--- src/components/AdaptiveCards.vue
+++ src/components/AdaptiveCards.vue
@@ -1,16 +1,14 @@
 <template>
-  <div id="cardHolder" />
+  <div class='light' />
 </template>
-<style scoped lang="scss">
-@import '../scss/adaptive-card';
-</style>
 
 <script>
 import * as AdaptiveCards from 'adaptivecards/dist/adaptivecards';
 import { Template, EvaluationContext } from 'adaptivecards-templating/dist/adaptivecards-templating';
-import axios from 'axios';
-import HostConfig from '../assets/exampleHostConfig.json';
-
+import SampleCard from '../assets/emptyCard.json'
+import lightConfig from '../assets/lightConfig.json'
+import darkConfig from '../assets/darkConfig.json'
+import * as d3 from 'd3-fetch'
 export default {
   name: 'AdaptiveCard',
   props: {
@@ -20,16 +18,10 @@ export default {
       required: false,
       default: null,
     },
-    options: {
-      type: Object | String,
-      required: false,
-      default() {
-        return {
-          useTemplating: false,
-          auth: false
-        }
-      },
-    },
+    height:String,
+    width:String,
+    useTemplating: Boolean,
+    showModal: Boolean,
     card: {
       type: Object | String,
       required: false,
@@ -46,6 +38,11 @@ export default {
       required: false,
       default: '',
     },
+    mode: {
+      type: String,
+      required: false,
+      default: 'light'
+    }
   },
   data() {
     return {
@@ -55,47 +52,50 @@ export default {
   },
   computed: {
     cardParsed() {
-      if (this.cardRemoteTemplate != null) return this.cardRemoteTemplate;
-      return typeof this.src === 'Object' ? JSON.stringify(this.card) : this.src;
+      if(this.src !== '' && this.card != null && this.card != ''){
+        throw new SyntaxError('Please only use src or card, never both!');
+        return;
+      }
+      if(this.useTemplating && ( this.data == null || this.data === '' )){
+        throw new SyntaxError('Data must be passed when using templating');
+        return;
+      }
+
+      if(this.src === '' && this.card == null || this.card === ''){
+        return SampleCard;
+      }
+
+      // Try to load from url
+      if(this.validURL(this.src)){
+        fetch(this.src, {
+          mode:'no-cors'
+        }).then(function(response) {
+            if (response.type === 'opaque' || response.ok) {
+              console.log(response)
+            } else {
+              console.log(response)
+            }
+        });
+      }
+
+     // if (this.cardRemoteTemplate != null) return this.cardRemoteTemplate;
+     // return typeof this.src === 'Object' ? JSON.stringify(this.card) : this.src;
     },
     dataParsed() {
-      return typeof this.data === 'Object' ? JSON.stringify(this.data) : this.data;
+      return this.data != null && this.data != undefined && typeof this.data === 'Object' ? JSON.stringify(this.data) : this.data;
     },
+    configParsed() {
+      return this.mode === 'light' ? lightConfig : darkConfig;
+    }
+
   },
   mounted() {
-    // Check a few things first
-    if(this.src !== '' && this.card != null && this.card != ''){
-      throw new SyntaxError('Please only use src or card, never both!');
-      return;
-    }
-    if(this.options.useTemplating && ( this.data == null || this.data === '' )){
-      throw new SyntaxError('Data must be passed when using templating');
-      return;
-    }
-
-
-
-// Check type of src
-    if(this.src.type === Object) {
-      console.log("CardObject ->" + this.src)
-    } else{
-      var valid = this.validURL(this.src);
-      console.log('Source valid -> ' + valid)
-      console.log(this.src);
-    }
-
-    console.log(this.config.type);
-    if(this.config.type === Object) {
-      console.log("CardObject ->" + this.config)
-    } else {
-      var valid = this.validURL(this.config);
-      console.log('Config valid -> ' + valid)
-      console.log(this.config);
-    }
-
     console.log(this.data);
+    console.log(this.cardParsed);
+    console.log(this.dataParsed);
     console.log(this.options);
-    //this.renderCard();
+    console.log(this.config);
+    this.renderCard();
   },
   methods: {
     validURL(str) {
@@ -108,24 +108,10 @@ export default {
       return !!pattern.test(str);
     },
     renderCard() {
-      const instance = axios.create({
-        paramsSerializer(params) {
-          return stringify(params, { arrayFormat: 'brackets' });
-        },
-      });
-
-      // Load the card from url if passed
-      if (this.src != '') {
-        const json = axios.get(this.src).then((data) => {
-          if (data.status == 200) {
-            this.cardRemoteTemplate = JSON.stringify(data.data);
-            console.log(this.cardRemoteTemplate);
-          }
-        });
-      }
+      try{
       this.cardHolder = new AdaptiveCards.AdaptiveCard();
       // Use Default Host Config if not passed
-      this.cardHolder.HostConfig = this.hostConfig == '' ? JSON.stringify(HostConfig) : this.hostConfig;
+      this.cardHolder.HostConfig = this.configParsed;
 
       if (this.useTemplating && this.data == null) {
         this.$el.remove();
@@ -140,7 +126,7 @@ export default {
 
         this.cardHolder.parse(cardToRender);
       } else {
-        this.cardHolder.parse(cardToRender);
+        this.cardHolder.parse(this.cardParsed);
       }
 
       this.cardHolder.onExecuteAction = (action) => {
@@ -148,9 +134,15 @@ export default {
       };
 
       this.cardElement = this.cardHolder.render();
-      var el = document.getElementById('cardHolder');
-      el.innerHTML = '';
-      el.appendChild(this.cardElement);
+      this.$el.innerHTML = '';
+      this.$el.appendChild(this.cardElement);
+      if(this.width != '') this.$el.style.width = `${this.width}`;
+      if(this.height != '') this.$el.style.height = `${this.height}`;
+      }
+      catch (ex) {
+        throw new Error('Could not render card: ' + ex);
+      }
+
     },
   },
   watch: {
@@ -170,3 +162,211 @@ export default {
 };
 
 </script>
+
+<style scoped>
+
+ .ac-media-poster {}
+
+ .ac-media-poster .empty {
+    height: 200px;
+    background-color: #F2F2F2;
+}
+
+ .th, td  {
+    border:none !important;
+}
+
+ .ac-fact-title,
+ .ac-fact-value{
+    border: none !important;
+}
+
+ .ac-media-playButton {
+    width: 56px;
+    height: 56px;
+    border: 1px solid #EEEEEE;
+    border-radius: 28px;
+    box-shadow: 0px 0px 10px #EEEEEE;
+    background-color: rgba(255, 255, 255, 0 .9);
+    color: black;
+    cursor: pointer;
+}
+
+ .ac-media-playButton-arrow {
+    color: black;
+}
+
+ .ac-media-playButton:hover {
+    background-color: white;
+}
+
+ .ac-image .ac-selectable {
+    cursor: pointer;
+}
+
+ .ac-image .ac-selectable:hover {
+    background-color: rgba(0, 0, 0, 0 .1);
+}
+
+ .ac-image .ac-selectable:active {
+    background-color: rgba(0, 0, 0, 0 .15);
+}
+
+a .ac-anchor {
+    text-decoration: none;
+}
+
+a .ac-anchor:link {
+    color: #6264A7;
+}
+
+a .ac-anchor:visited {
+    color: #6264A7;
+}
+
+a .ac-anchor:link:active {
+    color: #6264A7;
+}
+
+a .ac-anchor:visited:active {
+    color: #6264A7;
+}
+
+ .ac-container .ac-selectable,  .ac-columnSet .ac-selectable {
+    padding: 0px;
+}
+
+ .ac-container th{
+    border:none !important;
+}
+
+ .ac-container th,td{
+    border:none !important;
+}
+
+ .ac-container tr{
+    border:none !important;
+}
+
+ .ac-container .ac-selectable:hover,  .ac-columnSet .ac-selectable:hover {
+    background-color: rgba(0, 0, 0, 0 .1) !important;
+}
+
+ .ac-container .ac-selectable:active,  .ac-columnSet .ac-selectable:active {
+    background-color: rgba(0, 0, 0, 0 .15) !important;
+}
+
+ .ac-pushButton {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+    vertical-align: middle;
+    cursor: default;
+    font-family: "Segoe UI", sans-serif;
+    font-size: 14px;
+    padding: 4px 10px 5px 10px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    height: 32px;
+    background-color: white;
+    color: #6264A7;
+    border: 2px solid #BFC0DA;
+    border-radius: 4px;
+}
+
+ .ac-pushButton:hover {
+    background-color: #6264A7;
+    border: 2px solid #6264A7;
+    color: white;
+}
+
+ .ac-pushButton:active {
+    background-color: #6264A7;
+    border: 2px solid #6264A7;
+    color: white;
+}
+
+ .ac-pushButton .expanded {
+    background-color: #6264A7;
+    border: 2px solid #6264A7;
+    color: white;
+}
+
+ .ac-pushButton .style-emphasis {
+    background-color: #6264A7;
+    border: 2px solid #6264A7;
+    color: white;
+}
+
+ .ac-input {
+    font-family: "Segoe UI", sans-serif;
+    font-size: 14px;
+    color: black;
+}
+
+ .ac-input .ac-input-required {
+    background-image: linear-gradient(45deg, transparent, transparent 50%, red 50%, red 100%);
+    background-position: top right;
+    background-size:  .5em  .5em;
+    background-repeat: no-repeat;
+}
+
+ .ac-input .ac-input-validation-failed {
+    border: 1px solid red !important;
+}
+
+ .ac-input .ac-textInput {
+    resize: none;
+}
+
+ .ac-input .ac-textInput .ac-multiline {
+    height: 72px;
+}
+
+ .ac-input .ac-textInput,  .ac-input .ac-numberInput,  .ac-input .ac-dateInput,  .ac-input .ac-timeInput {
+    height: 31px;
+}
+
+ .ac-input .ac-textInput,  .ac-input .ac-numberInput,  .ac-input .ac-dateInput,  .ac-input .ac-timeInput,  .ac-input .ac-multichoiceInput {
+    background-color: #F3F2F1;
+    border: 1px solid #F3F2F1;
+    border-radius: 4px;
+    padding: 4px 8px 4px 8px;
+}
+
+/* ac-inlineActionButton should set height to the same as ac-input .ac-textInput */
+
+ .ac-inlineActionButton {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    font-family: "Segoe UI", sans-serif;
+    font-size: 14px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    border: none;
+    background-color: transparent;
+    height: 31px;
+}
+
+ .ac-inlineActionButton .textOnly {
+    padding: 0 8px;
+}
+
+ .ac-inlineActionButton .iconOnly {
+    padding: 0;
+}
+
+ .ac-inlineActionButton:hover {
+    background-color: #EEEEEE;
+}
+
+ .ac-inlineActionButton:active {
+    background-color: #CCCCCC;
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,13 @@
 import Vue from 'vue';
 import vueCustomElement from 'vue-custom-element';
 import Modal from './components/Modal.vue';
+
 Vue.component('modal', Modal);
 
 Vue.use(vueCustomElement);
 
 const customElementOptions = {
   shadow: true,
-  shadowCss: `
-      .root{
-          all: initial;
-          font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-          font-size: 13px;
-          -webkit-font-smoothing: antialiased;
-      }
-
-      a{
-          text-decoration: none;
-          color: unset;
-      }
-      a:hover{
-          color: #63a9f9;
-      }
-      `,
 };
 const load = (loader) => async () => (await loader()).default;
 Vue.config.ignoredElements = ['adaptive-card'];
